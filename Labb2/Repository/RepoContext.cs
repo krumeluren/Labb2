024bcca723565ce418c7c8ab721651247a56922b
krumeluren <9928liqv@gmail.com>
2025-12-03 12:08:19 +0100

    RepoContext

--- Labb2/Repository/RepoContext.cs
+++ Labb2/Repository/RepoContext.cs
@@ -3,13 +3,9 @@ using Microsoft.EntityFrameworkCore;
 public class RepoContext : DbContext {
     public RepoContext (DbContextOptions options) : base(options) { }
 
-    protected override void OnModelCreating (ModelBuilder modelBuilder) {
-
-    }
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Album> Albums { get; set; }
     public DbSet<Song> Songs { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
-
 }
